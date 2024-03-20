776aefb9b874555729f239784649e71d925bd26d
INAUGURATE-Ryong <dksdndyd123@gmail.com>
2024-03-20 14:41:09 +0900

    [add] Client, Pps Entity

---
+++ BPS/src/main/java/com/sales/BPS/msales/entity/Client.java
@@ -0,0 +1,59 @@
+package com.sales.BPS.msales.entity;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+import static jakarta.persistence.FetchType.LAZY;
+
+@Entity
+@Data
+public class Client {
+    @Id
+    @Column(name = "client_code")
+    private String clientCode;
+
+    @Column(name= "client_name")
+    private String clientName;
+
+    @Column(name = "client_class")
+    private Integer clientClass;
+
+    @Column(name="client_boss")
+    private String clientBoss;
+
+    @Column(name="client_where")
+    private String clientWhere;
+
+    @Column(name="client_post")
+    private String clientPost;
+
+    @Column(name="client_emp")
+    private String clientEmp;
+
+    @Column(name="client_emptel")
+    private String clientEmpTel;
+
+    @Column(name="client_start")
+    private LocalDateTime clientStart;
+
+    @Column(name = "client_end")
+    private LocalDateTime clientEnd;
+
+    @Column(name = "client_note")
+    private String clientNote;
+
+    @ManyToOne(fetch = LAZY)
+    @JoinColumn(name = "emp_code")
+    private Employee employee;
+
+    @OneToMany(mappedBy = "client")
+    private List<Ppc> ppcs = new ArrayList<>();
+
+
+
+
+}

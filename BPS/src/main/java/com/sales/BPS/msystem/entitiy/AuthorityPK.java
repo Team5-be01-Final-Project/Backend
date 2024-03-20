f344e2ec93d8fc25c349bdc43d22a3173dcf0725
dhkdtld37 <hyeonnls96@naver.com>
2024-03-20 16:46:08 +0900

    [Fix] AuthorityPK에 Data 어노테이션 추가

--- BPS/src/main/java/com/sales/BPS/msystem/entitiy/AuthorityPK.java
+++ BPS/src/main/java/com/sales/BPS/msystem/entitiy/AuthorityPK.java
@@ -3,6 +3,8 @@ package com.sales.BPS.msystem.entitiy;
 import lombok.Data;
 
 import java.io.Serializable;
+
+@Data
 public class AuthorityPK implements Serializable {
 
     private Integer empCode;

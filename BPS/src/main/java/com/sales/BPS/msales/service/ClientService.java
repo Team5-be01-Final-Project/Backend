c2a536d51916f7d9b2a670aabf0ea1082acf1a60
INAUGURATE-Ryong <dksdndyd123@gmail.com>
2024-03-21 02:17:13 +0900

    [add,fix]거래처 등록 기능 추가 및 ENTITY 수정

---
+++ BPS/src/main/java/com/sales/BPS/msales/service/ClientService.java
@@ -0,0 +1,23 @@
+package com.sales.BPS.msales.service;
+
+import com.sales.BPS.msales.entity.Client;
+import com.sales.BPS.msales.repository.ClientRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ClientService {
+
+    private final ClientRepository clientrepository;
+
+    @Autowired
+    public ClientService(ClientRepository clientrepository) {
+        this.clientrepository = clientrepository;
+    }
+
+    public Client saveClient(Client client){
+        return clientrepository.save(client);
+    }
+
+
+}

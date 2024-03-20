f6bf6cd599f193772efb97115538eb1aaefb3545
dhkdtld37 <hyeonnls96@naver.com>
2024-03-20 12:05:56 +0900

    [add] test 파일 재생성

---
+++ BPS/src/main/java/com/sales/BPS/msales/test.java
@@ -0,0 +1,7 @@
+package com.sales.BPS.msales;
+
+public class test {
+    public static void main(String[] args) {
+        System.out.println("this is test. please delete this file and add new file");
+    }
+}

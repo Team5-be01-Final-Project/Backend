06631531d4e158531cc0ce6f02c8a43b6471f78d
INAUGURATE-Ryong <dksdndyd123@gmail.com>
2024-03-21 05:10:04 +0900

    [fix]리스트 조회 기술 수정

--- BPS/src/main/java/com/sales/BPS/msales/repository/ClientRepository.java
+++ BPS/src/main/java/com/sales/BPS/msales/repository/ClientRepository.java
@@ -1,6 +1,5 @@
 package com.sales.BPS.msales.repository;
 
-import com.sales.BPS.msales.dto.ClientDto;
 import com.sales.BPS.msales.entity.Client;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
@@ -10,7 +9,16 @@ import java.util.List;
 public interface ClientRepository extends JpaRepository<Client,String> {
 
     @Query("SELECT c.clientCode AS clientCode, c.clientName AS clientName, c.clientClass AS clientClass, c.clientBoss AS clientBoss, c.clientWhere AS clientWhere, c.clientPost AS clientPost, c.clientEmp AS clientEmp, c.clientEmpTel AS clientEmpTel FROM Client c")
-    List<ClientDto> findClientsWithSpecificFields();
+    List<ClientProjection> findClientsWithSpecificFields();
 
-
+    interface ClientProjection {
+        String getClientCode();
+        String getClientName();
+        Integer getClientClass();
+        String getClientBoss();
+        String getClientWhere();
+        String getClientPost();
+        String getClientEmp();
+        String getClientEmpTel();
+    }
 }

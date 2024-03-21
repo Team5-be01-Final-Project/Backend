676cd393a6d39b13f7e558553d8ba71ff7efa53e
jsmin6330 <jsmin6330@gmail.com>
2024-03-21 17:15:38 +0900

    [Fix] 환경 변수 수정

--- start.sh
+++ start.sh
@@ -1,5 +1,11 @@
 #!/usr/bin/env bash
 
+echo "==========================="
+echo "DB_URL: ${DB_URL}"
+echo "DB_USERNAME: ${DB_USERNAME}"
+echo "DB_PASSWORD:${DB_PASSWORD}"
+echo "==========================="
+
 cd /home/ubuntu/app/bps/BPS
 
 mvn clean install -DskipTests

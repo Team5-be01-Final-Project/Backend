a8ff5080203d44f0c5a970772ebb48c68aa9f563
jsmin6330 <jsmin6330@gmail.com>
2024-03-21 20:47:01 +0900

    [Feat] 유저 조회 기능 추가

---
+++ BPS/src/main/java/com/sales/BPS/msystem/controller/EmployeeController.java
@@ -0,0 +1,28 @@
+package com.sales.BPS.msystem.controller;
+
+import com.sales.BPS.msystem.entity.Employee;
+import com.sales.BPS.msystem.service.EmployeeService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+
+@RestController
+@RequestMapping("/employees")
+public class EmployeeController {
+    private final EmployeeService employeeService;
+
+    @Autowired
+    public EmployeeController(EmployeeService employeeService) {
+        this.employeeService = employeeService;
+    }
+
+    @GetMapping
+    public List<Employee> getAllEmployees(){
+        return employeeService.findAll();
+    }
+}
--- BPS/src/main/java/com/sales/BPS/msystem/repository/EmployeeRepository.java
+++ BPS/src/main/java/com/sales/BPS/msystem/repository/EmployeeRepository.java
@@ -2,6 +2,14 @@ package com.sales.BPS.msystem.repository;
 
 import com.sales.BPS.msystem.entity.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
-public interface EmployeeRepository extends JpaRepository<Employee, String> {
+import java.util.List;
+
+public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
+
+
+    List<Employee> findAll();
+
+
 }

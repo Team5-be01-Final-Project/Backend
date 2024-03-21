46b380d0e3cd7310e19faee382d79093dadac60c
dhkdtld37 <hyeonnls96@naver.com>
2024-03-21 16:47:30 +0900

    [Add] 사원 로그인 및 리스트조회 파일 추가

--- BPS/src/main/java/com/sales/BPS/msystem/controller/LoginController.java
+++ BPS/src/main/java/com/sales/BPS/msystem/controller/LoginController.java
@@ -9,54 +9,45 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-import com.test.entity.Business;
-import com.test.service.BusinessService;
+import com.sales.BPS.msystem.entity.Employee;
+import com.sales.BPS.msystem.service.EmployeeService;
 
 import jakarta.servlet.http.HttpSession;
 
+import java.util.List;
+
 @Controller
-@RequestMapping("/business")
-public class BusinessLoginController {
+@RequestMapping("/Bps")
+public class LoginController {
 
     @Autowired
-    private BusinessService businessService;
-
-    @GetMapping("/login")
-    public String showLoginForm() {
-        return "Blogin_form";
-    }
+    private EmployeeService employeeService;
 
     @PostMapping("/login")
-    public String Blogin(@RequestParam String bId, @RequestParam String bpwd, Model model, HttpSession session) {
-        boolean isLogin = businessService.loginBusiness(bId, bpwd);
+    public String employeeLogin(@RequestParam Integer empCode, @RequestParam String empPw, Model model, HttpSession session) {
+        boolean isLogin = employeeService.loginEmployee(empCode, empPw);
 
         if (isLogin) {
-            session.setAttribute("bId", bId);
-            System.out.println("session 정보: " + (String) session.getAttribute("bId"));
-            return "BforPrivate"; // 로그인 성공시 유저 메뉴로 이동
+            session.setAttribute("empCode", empCode);
+            return "redirect:/list";
         } else {
-            model.addAttribute("error", "로그인을 실패했습니다.");
-            return "Blogin_form"; // 로그인 실패 시 로그인 폼을 다시 표시
+            model.addAttribute("error", "Failed to login.");
+            return "login";
         }
     }
 
     @GetMapping("/logout")
-    public String logoutBusiness(HttpSession session) {
-        session.invalidate(); // 세션 무효화
-        return "redirect:/business/login"; //로그인 페이지로 이동
+    public String logoutEmployee(HttpSession session) {
+        session.invalidate();
+        return "redirect:/login";
     }
 
-    @GetMapping("/BforPrivate")
-    public String showBforPrivate(Model model, HttpSession session) {
-        String bId = (String) session.getAttribute("bId");
-
-        if (bId != null) {
-            Business currentbusiness = businessService.getBusinessByCode(bId);
-            model.addAttribute("business", currentbusiness);
-        } else {
-            System.out.println("유효하지 않은 접근입니다.");
-            return "redirect:/business/login"; //로그인 페이지로 이동
-        }
-        return "BforPrivate";
+    @GetMapping("/list")
+    public String listEmployees(Model model) {
+        List<Employee> employees = employeeService.listAllEmployee();
+        model.addAttribute("employees", employees);
+        return "employee_list";
     }
+
+
 }
--- BPS/src/main/java/com/sales/BPS/msystem/service/EmployeeService.java
+++ BPS/src/main/java/com/sales/BPS/msystem/service/EmployeeService.java
@@ -6,20 +6,7 @@ import com.sales.BPS.msystem.entity.Employee;
 
 public interface EmployeeService {
 
-    List<Employee> (String bId);
-    Employee getEmployeeByCode(String employeeId);
-    Employee updateEmployee(String employeeId, Employee updatedEmployee);
-    void deleteEmployee(String employeeId);
-    boolean loginEmployee(String employeeId, String bpwd);
-
-    // 포스트맨
-    Business saveB(Business business);					// 사업자 추가
-    Business getBByCode(String bcode); 					// 사업자 정보 보기
-    List<Business> getAllB();							// 전체 사업자 보기
-    Business updateB(String bcode, Business business);	// 사업자 정보 업데이트
-    void deleteBByCode(String bcode); 					// 사업자 삭제
+    boolean loginEmployee(Integer empCode, String empPw);            // login
+    List<Employee> listAllEmployee();                          // list
 
 }
-
-
-

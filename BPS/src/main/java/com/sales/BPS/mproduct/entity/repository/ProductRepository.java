69a197dd1bddad2f40dc468f6395dcfbf939e41b
parc02 <pascalmin49@gmail.com>
2024-03-21 12:23:50 +0900

    [add] add service

--- BPS/src/main/java/com/sales/BPS/mproduct/entity/repository/ProductRepository.java
+++ BPS/src/main/java/com/sales/BPS/mproduct/entity/repository/ProductRepository.java
@@ -1,8 +1,24 @@
 package com.sales.BPS.mproduct.entity.repository;
 
 import com.sales.BPS.mproduct.entity.Product;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+import java.util.Optional;
+
 public interface ProductRepository extends JpaRepository <Product, Integer> {
+
+    Page<Product> findAll(Pageable pageable);
+
+    // Search By Production Code
+    Optional<Product> findByProCode (Integer proCode);
+
+    List<Product> findByProName(String proName);
+
+    List<Product> findByProSeg(String proSeg);
+
+    List<Product> findByProIngre(String
 
 }
---
+++ BPS/src/main/java/com/sales/BPS/mproduct/entity/service/ProductService.java
@@ -0,0 +1,18 @@
+package com.sales.BPS.mproduct.entity.service;
+
+import com.sales.BPS.mproduct.entity.repository.ProductRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ProductService {
+    private final ProductRepository productRepository;
+
+    @Autowired
+    public ProductService(ProductRepository productRepository) {
+        this.productRepository = productRepository;
+    }
+
+
+
+}

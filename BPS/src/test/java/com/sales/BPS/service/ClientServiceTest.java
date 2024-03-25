9e9fdf698878ecbdab11480f3536d86cbdfc023b
INAUGURATE-Ryong <dksdndyd123@gmail.com>
2024-03-25 14:12:28 +0900

    [Feat] 4.10/test/WY 단위test 코드 작성

---
+++ BPS/src/test/java/com/sales/BPS/service/ClientServiceTest.java
@@ -0,0 +1,10 @@
+package com.sales.BPS.service;
+
+import jakarta.transaction.Transactional;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@ExtendWith(MockitoExtension.class)
+public class ClientServiceTest {
+}

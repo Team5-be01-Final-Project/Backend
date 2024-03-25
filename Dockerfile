f5700b6489fc831a5c0c056d257ed342a9de18f4
jsmin6330 <jsmin6330@gmail.com>
2024-03-25 16:17:29 +0900

    [Fix] git deploy 동작 수정

--- Dockerfile
+++ Dockerfile
@@ -1,8 +1,5 @@
 FROM eclipse-temurin:17-jdk-alpine
 
-ENV DB_URL=""
-ENV DB_USERNAME=""
-ENV DB_PASSWORD=""
+COPY *.jar app.jar
 
-COPY BPS/target/*.jar app.jar
 ENTRYPOINT ["java","-jar","/app.jar"]
--- start.sh
+++ start.sh
@@ -1,15 +1,5 @@
 #!/usr/bin/env bash
 
-echo "==========================="
-echo "DB_URL: ${DB_URL}"
-echo "DB_USERNAME: ${DB_USERNAME}"
-echo "DB_PASSWORD:${DB_PASSWORD}"
-echo "==========================="
-
-cd /home/ubuntu/app/bps/BPS
-
-mvn clean install -DskipTests
-
-cd ..
+cd /home/ubuntu/app/bps
 
 sudo -E docker compose -f docker-compose.yml up -d

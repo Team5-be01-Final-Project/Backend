45e5e4cfcf9479364790e3647bf77af2f8d4335e
dhkdtld37 <hyeonnls96@naver.com>
2024-03-26 14:11:34 +0900

    [Add] 재고조회 및 검색 기능 구현

--- BPS/src/main/java/com/sales/BPS/mproduct/controller/StockController.java
+++ BPS/src/main/java/com/sales/BPS/mproduct/controller/StockController.java
@@ -1,4 +1,36 @@
 package com.sales.BPS.mproduct.controller;
 
+import com.sales.BPS.mproduct.dto.StockListDTO;
+import com.sales.BPS.mproduct.service.StockService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/stocks")
 public class StockController {
+
+    private final StockService stockService;
+
+    @Autowired
+    public StockController(StockService stockService) {
+        this.stockService = stockService;
+    }
+
+    @GetMapping("/all")
+    public List<StockListDTO> findAllStockList() {
+        return stockService.findAllStockList();
+    }
+
+    @GetMapping("/search")
+    public List<StockListDTO> findStockListByCondition(@RequestParam(required = false) Integer proCode,
+                                                       @RequestParam(required = false) String proName) {
+        return stockService.findStockListByCondition(proCode, proName);
+    }
+
+
 }
--- BPS/src/main/java/com/sales/BPS/mproduct/repository/StockRepository.java
+++ BPS/src/main/java/com/sales/BPS/mproduct/repository/StockRepository.java
@@ -1,4 +1,26 @@
 package com.sales.BPS.mproduct.repository;
 
-public class StockRepository {
+import com.sales.BPS.mproduct.dto.StockListDTO;
+import com.sales.BPS.mproduct.entity.Stock;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
+
+public interface StockRepository extends JpaRepository<Stock, Integer> {
+
+    @Query("SELECT new com.sales.BPS.mproduct.dto.StockListDTO(p.proCode, p.proName, s.stoAmo, p.proUnit) " +
+            "FROM Product p " +
+            "JOIN Stock s ON p.proCode = s.proCode")
+    List<StockListDTO> AllStockList();
+
+    @Query("SELECT new com.sales.BPS.mproduct.dto.StockListDTO(p.proCode, p.proName, s.stoAmo, p.proUnit) "+
+            "FROM Product p " +
+            "JOIN Stock s ON p.proCode = s.proCode " +
+            "WHERE (:proCode IS NULL OR CAST(p.proCode AS string) LIKE %:proCode%)" +
+            "AND (:proName IS NULL OR p.proName LIKE %:proName%)")
+    List<StockListDTO> findStockList(@Param("proCode") Integer proCode,
+                                     @Param("proName") String proName);
+
 }

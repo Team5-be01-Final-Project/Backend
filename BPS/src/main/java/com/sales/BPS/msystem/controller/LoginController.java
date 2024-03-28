bb65b551cc76969afc292369d88b0848d7247297
jsmin6330 <jsmin6330@gmail.com>
2024-03-28 13:59:43 +0900

    [Feat] Cookie 기능 추가

--- BPS/src/main/java/com/sales/BPS/msystem/controller/LoginController.java
+++ BPS/src/main/java/com/sales/BPS/msystem/controller/LoginController.java
@@ -1,6 +1,11 @@
 package com.sales.BPS.msystem.controller;
 
+import com.sales.BPS.msystem.dto.LoginRequestDTO;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+import lombok.Data;
 import org.springframework.beans.factory.annotation.Autowired;
+import jakarta.servlet.http.Cookie;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -14,15 +19,19 @@ public class LoginController {
     private EmployeeService employeeService;
 
     @PostMapping("/login")
-    public ResponseEntity<?> login(@RequestParam Integer empCode, @RequestParam String password) {
-        boolean isValidUser = employeeService.loginEmployee(empCode, password);
+    public ResponseEntity<?> login(@RequestBody LoginRequestDTO loginRequest, HttpServletResponse response) {
+
+        System.out.println(loginRequest.getEmpCode()+","+loginRequest.getEmpPw());
+
+        boolean isValidUser = employeeService.loginEmployee(loginRequest.getEmpCode(), loginRequest.getEmpPw());
         if (isValidUser) {
+            Cookie empCodeCookie = new Cookie("empCode", loginRequest.getEmpCode().toString());
+            response.addCookie(empCodeCookie);
             return ResponseEntity.ok("Login successful");
         } else {
             return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Invalid credentials");
         }
     }
-
     @PostMapping("/login-error")
     public ResponseEntity<?> loginError() {
         return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Login failed");

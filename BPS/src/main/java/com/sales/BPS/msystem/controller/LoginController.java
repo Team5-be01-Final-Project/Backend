337cc7e9ecede42b0b1414638d12b80ecab009f1
jsmin6330 <jsmin6330@gmail.com>
2024-03-28 15:24:58 +0900

    [Feat] 로그아웃 기능 추가

--- BPS/src/main/java/com/sales/BPS/msystem/controller/LoginController.java
+++ BPS/src/main/java/com/sales/BPS/msystem/controller/LoginController.java
@@ -1,6 +1,7 @@
 package com.sales.BPS.msystem.controller;
 
 import com.sales.BPS.msystem.dto.LoginRequestDTO;
+import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.servlet.http.HttpSession;
 import lombok.Data;
@@ -26,6 +27,8 @@ public class LoginController {
         boolean isValidUser = employeeService.loginEmployee(loginRequest.getEmpCode(), loginRequest.getEmpPw());
         if (isValidUser) {
             Cookie empCodeCookie = new Cookie("empCode", loginRequest.getEmpCode().toString());
+            empCodeCookie.setMaxAge(12 * 60 * 60); // 쿠키의 유효 기간을 12시간으로 설정
+            empCodeCookie.setPath("/"); // 쿠키의 경로 설정(모든 경로에서 쿠키 사용 가능)
             response.addCookie(empCodeCookie);
             return ResponseEntity.ok("Login successful");
         } else {
@@ -36,4 +39,17 @@ public class LoginController {
     public ResponseEntity<?> loginError() {
         return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Login failed");
     }
+
+    @PostMapping("/logout")
+    public ResponseEntity<?> logout(HttpServletResponse response) {
+        // 쿠키 무효화
+        Cookie cookie = new Cookie("empCode", null); //empCode 쿠키 만료
+        cookie.setPath("/"); // 쿠키의 경로 설정
+        cookie.setMaxAge(0); // 쿠키의 만료 시간을 0으로 설정하여 즉시 만료
+        response.addCookie(cookie);
+
+
+        return ResponseEntity.ok("Logout successful");
+    }
+
 }

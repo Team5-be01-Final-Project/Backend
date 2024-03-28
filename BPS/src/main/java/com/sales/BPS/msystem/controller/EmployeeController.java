f111b231d5850ebb22fb9aa2f9f23337e1b20d5b
INAUGURATE-Ryong <dksdndyd123@gmail.com>
2024-03-28 15:36:30 +0900

    [add] API테스트 Swagger추가

--- BPS/src/main/java/com/sales/BPS/msystem/controller/EmployeeController.java
+++ BPS/src/main/java/com/sales/BPS/msystem/controller/EmployeeController.java
@@ -3,6 +3,8 @@ package com.sales.BPS.msystem.controller;
 import com.sales.BPS.msystem.dto.EmployeeInfoDTO;
 import com.sales.BPS.msystem.entity.Employee;
 import com.sales.BPS.msystem.service.EmployeeService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -15,6 +17,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/employees")
+@Tag(name = "System API", description = "시스템관리 API입니다.")
 public class EmployeeController {
     private final EmployeeService employeeService;
 
@@ -29,6 +32,8 @@ public class EmployeeController {
 //    }
 
     @GetMapping("/list")
+    @Tag(name = "System API")
+    @Operation(summary = "사원정보 조회",description = "사원정보(부서이름,이름, 전화번호 조회")
     public List<EmployeeInfoDTO> searchEmployees(@RequestParam(required = false) String deptName,
                                                  @RequestParam(required = false) String empName,
                                                  @RequestParam(required = false) String empTel) {
--- BPS/src/main/java/com/sales/BPS/msystem/controller/LoginController.java
+++ BPS/src/main/java/com/sales/BPS/msystem/controller/LoginController.java
@@ -1,5 +1,7 @@
 package com.sales.BPS.msystem.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -8,12 +10,15 @@ import com.sales.BPS.msystem.service.EmployeeService;
 
 @RestController
 @RequestMapping("/api")
+@Tag(name = "System API", description = "시스템관리 API입니다.")
 public class LoginController {
 
     @Autowired
     private EmployeeService employeeService;
 
     @PostMapping("/login")
+    @Tag(name = "System API")
+    @Operation(summary = "로그인",description = "로그인 시도 후 결과 반환")
     public ResponseEntity<?> login(@RequestParam Integer empCode, @RequestParam String password) {
         boolean isValidUser = employeeService.loginEmployee(empCode, password);
         if (isValidUser) {
@@ -24,6 +29,8 @@ public class LoginController {
     }
 
     @PostMapping("/login-error")
+    @Tag(name = "System API")
+    @Operation(summary = "로그인",description = "로그인 시도 후 실패 전달")
     public ResponseEntity<?> loginError() {
         return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Login failed");
     }
